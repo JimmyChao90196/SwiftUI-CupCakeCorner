014c0bed971726aa70ccc9da8dbb953c74724d30
JimmyChao <myname90196@gmail.com>
2024-04-18 09:44:58 +0800

    -feat- 1. Learn how to use async and await in SwiftUI

--- SwfitUI-CupcakeCorner/ContentView.swift
+++ SwfitUI-CupcakeCorner/ContentView.swift
@@ -7,15 +7,49 @@
 
 import SwiftUI
 
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
+}
+
 struct ContentView: View {
+    
+    @State var results = [Result]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List(results, id: \.trackId) { result in
+            VStack(alignment: .leading) {
+                Text("\(result.trackName)")
+                    .font(.headline)
+                Text("\(result.collectionName)")
+            }
+        }.task {
+            await loadData()
         }
-        .padding()
+    }
+    
+    // Load data function
+    func loadData() async {
+        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+            print("Invalid data")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let response = try? JSONDecoder().decode(Response.self, from: data)
+            if let response {
+                results = response.results
+            }
+            
+        } catch {
+            print(error)
+        }
     }
 }
 
