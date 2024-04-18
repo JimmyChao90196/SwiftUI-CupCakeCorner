16c022dd63cf3abda305a47c884ef16b60ac5bb5
JimmyChao <myname90196@gmail.com>
2024-04-18 09:18:50 +0800

    Initial Commit

---
+++ SwfitUI-CupcakeCorner/SwfitUI_CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwfitUI_CupcakeCornerApp.swift
+//  SwfitUI-CupcakeCorner
+//
+//  Created by JimmyChao on 2024/4/18.
+//
+
+import SwiftUI
+
+@main
+struct SwfitUI_CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
